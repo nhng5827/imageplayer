7e1fb71190b95afc4f7074ab63643517e74a7b67
Nhi Nguyen <nhng5827@colorado.edu>
2020-02-24 15:20:30 -0700

    update scrubber to handle drag, add speed up and speed down on animation, update buttons

--- js/jquery.imageplayer.js
+++ js/jquery.imageplayer.js
@@ -1,7 +1,6 @@
 if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
 
 (function ($) {
-    
     $.fn.extend({
         imagePlayer: function(options) {
             if (options && typeof(options) == 'object') {
@@ -26,7 +25,7 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
         var imagesEl = [];
         var imagesLoaded = 0;
         var body = null;
-        var player, stage, controls, start, prev, play_pause, next, end, scrubber, scrubber_handle, fullscreen, frame_count, caption, image = null;
+        var player, stage, controls, start, prev, play_pause, next, end, speed_up, speed_down, scrubber, scrubber_handle, fullscreen, frame_count, caption, image = null;
         var last_frame_scrubber_pos = 0;
         var full = false;
         var pauseOnHover = settings.pauseOnHover;
@@ -34,22 +33,21 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
         var i = 0; // current image
         var rotator = null;
         playlist.find('img').each(function() {
-            images.push(this.src);
+            images.push(this.dataset.src);
             imagesEl.push(this);
         });
-        
         if(images.length == 0) {
             throw "No images found!";
         }
 
         create_player();
-        
-        // Check if all images are loaded here. http://api.jquery.com/load-event/
-        $.each(imagesEl, function(index, el) {
+	// Check if all images are loaded here. http://api.jquery.com/load-event/
+	$.each(imagesEl, function(index, el){ 
+	    $(el).attr('src',el.dataset.src);
             $(el).load(function() {
                 imagesLoaded++;
                 if(imagesLoaded >= images.length) {
-                    if(settings.autoStart === true) {
+                    if(settings.autoStart == true) {
                         image_cycle();
                     } else {
                         set_image(images[0]);
@@ -59,22 +57,23 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             });
         });
 
-        function create_player() {
-            // Player elements.
-            player          = $('<div>').addClass('img_player');
+	function create_player() {
+            // Player elements.	
+	    player          = $('<div>').addClass('img_player');
             stage           = $('<div>').addClass('stage');
             controls        = $('<div>').addClass('controls');
             start           = $('<a>').attr('href', '#').addClass('start');
             prev            = $('<a>').attr('href', '#').addClass('prev');            
-            play_pause      = $('<a>').attr('href', '#');
+            play_pause      = $('<a id = "play_pause">').attr('href', '#');
             next            = $('<a>').attr('href', '#').addClass('next');
             end             = $('<a>').attr('href', '#').addClass('end');
-            scrubber        = $('<div>').addClass('scrubber');
-            scrubber_handle = $('<a>').attr('href', '#');
+            speed_up        = $('<a>').attr('href', '#').addClass('speed_up');
+            speed_down      = $('<a>').attr('href', '#').addClass('speed_down');
+            scrubber = $('<input id = "range" value="0" type="range" min="0" max="'+String(Number(settings.num_images) - 1)+'" role="input-range">').addClass('scrubber');
             fullscreen      = $('<a>').attr('href', '#').addClass('fullscreen');
             frame_count     = $('<span>').addClass('frame_count');
             caption         = $('<div>').addClass('caption');
-            // Set dimensions
+            // Set dimensions 
             player.css({
                 width:settings.stageWidth + 'px',
                 height:settings.stageHeight + 40 + 'px' // 40 is control bar height
@@ -86,24 +85,21 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             controls.css({
                 width:settings.stageWidth + 'px'
             });
-            scrubber.css({
-                width: Math.floor(((settings.stageWidth - 297) / images.length)*(images.length + 1)) + 'px'
-            });
             // Set the right control for play/pause.
             (settings.autoStart===true) ? play_pause.addClass('pause') : play_pause.addClass('play');
             // Build the player.
-            player.append(stage.append(caption)).append(controls.append(start).append(prev).append(play_pause).append(next).append(end).append(scrubber.append(scrubber_handle)).append(fullscreen).append(frame_count));         
+            player.append(stage.append(caption)).append(controls.append(start).append(prev).append(play_pause).append(next).append(end).append(scrubber).append(speed_down).append(speed_up).append(fullscreen).append(frame_count));         
             playlist.hide().after(player);
-            inc = Math.floor(scrubber.width() / images.length);
-        }
-        
-        function create_bindings() {
+            inc = 1
+         }
+
+	function create_bindings() {
             // Bind mouse interactions
             stage.bind('mouseenter', function(e) {
                 handle_image_hover(e, this);
             }).bind('mouseleave', function(e) {
                 handle_image_out(e, this);
-            }); // .hover seems not tow work?
+            }); 
             play_pause.bind('click', function(e) {
                 handle_control_click(e, this);
             });
@@ -119,11 +115,17 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             end.bind('click', function(e) {
                 handle_end_click(e, this);
             });
+            speed_up.bind('click', function(e) {
+                handle_speed_up(e, this);
+            });
+            speed_down.bind('click', function(e) {
+                handle_speed_down(e, this);
+            });
             fullscreen.bind('click', function(e) {
                 handle_fullscreen_click(e, this);
             });
-            scrubber.bind('click', function(e) {
-                handle_scrubber_click(e, this);
+            scrubber.bind('change', function(e) {
+                handle_scrubber_drag(e, this);
             });            
         }
         
@@ -156,9 +158,8 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             if(settings.loop === true) {
                 if (i > images.length - 1) {
                     i = 0;
-                    // stop animation
-                    scrubber_handle.stop(true, true);
-                    scrubber_handle.css('left', '0');
+                    handle = document.getElementById('range');
+                    handle.value = 0
                 }
             }
             if (i < images.length) {
@@ -169,21 +170,17 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
         
         function image_transition(img) {
             set_image(img);
-            // animate scrubber
-            last_frame_scrubber_pos = parseFloat(scrubber_handle.css('left'));
-            var remaining = inc*(i+1) - last_frame_scrubber_pos;
-            // var percent = Math.floor(remaining / inc);
-            scrubber_handle.stop(true, true);
-            scrubber_handle.animate({
-                left: '+='+remaining+'px'
-            }, settings.delay*1000, 'linear');
+            handle = document.getElementById('range');
+            //animate scrubber 
+            if( i != 0 ){
+              handle.stepUp(1);
+            }
             rotator = setTimeout(image_cycle, settings.delay * 1000);
         }
         
         function handle_image_hover(e, elem) {
-            if(pauseOnHover === true && play_pause.attr('class') === 'pause') { // is playing
+            if(pauseOnHover === true && play_pause.attr('class') === 'pause') { // is playing 
                 clearTimeout(rotator);
-                scrubber_handle.stop(true, true);  
             }
         }
         
@@ -197,11 +194,11 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             e.preventDefault();
             elem = $(elem, player);
             // try if we can use "hasClass"
-            if(elem.attr('class') == 'pause') { // it's playing (then pause)
+            if(elem.attr('class') == 'pause') { // it's playing (then pause) 
                 elem.attr('class', 'play');
                 clearTimeout(rotator);
-                scrubber_handle.stop(true, false);
-                scrubber_handle.css('left', last_frame_scrubber_pos + 'px');
+                handle = document.getElementById('range');
+                handle.value = handle.value - 1   
                 i--;
             } else { // paused (we have to resume playback)
                 image_cycle();
@@ -209,18 +206,19 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             }
         }
 
-		  // TODO: prev/next/start/end have a lot in common
-
+                 // TODO: prev/next/start/end have a lot in common
+        
+          
         function handle_prev_click(e, elem) {
             e.preventDefault();
             elem = $(elem, player);
             clearTimeout(rotator);
-            scrubber_handle.stop(true, false);
 			i = (i - 1 < 0) ? 0 : i - 1;
-            scrubber_handle.css('left', inc*i + 'px');
-            if(play_pause.attr('class') === 'pause') { // was playing
+            handle = document.getElementById('range');
+            handle.value = i  
+            if(play_pause.attr('class') === 'pause') { 
                 image_cycle();
-            } else {
+            } else { // was playing
                 set_image(images[i]);
             }
         }
@@ -229,12 +227,12 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             e.preventDefault();
             elem = $(elem, player);
             clearTimeout(rotator);
-            scrubber_handle.stop(true, false);
 			i = (i + 1 > images.length - 1) ? images.length - 1 : i + 1;
-            scrubber_handle.css('left', inc*i + 'px');
-            if(play_pause.attr('class') === 'pause') { // was playing
+            handle = document.getElementById('range');
+            handle.value = i  
+            if(play_pause.attr('class') === 'pause') { 
                 image_cycle();
-            } else {
+            } else { // was playing
                 set_image(images[i]);
             }
         }
@@ -243,12 +241,12 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             e.preventDefault();
             elem = $(elem, player);
             clearTimeout(rotator);
-            scrubber_handle.stop(true, false);
             i = 0;
-            scrubber_handle.css('left', '0px');
-            if(play_pause.attr('class') === 'pause') { // was playing
+            handle = document.getElementById('range');
+            handle.value = 0  
+            if(play_pause.attr('class') === 'pause') { 
                 image_cycle();
-            } else {
+            } else { // was playing
                 set_image(images[i]);
             }
         }
@@ -257,21 +255,21 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             e.preventDefault();
             elem = $(elem, player);
             clearTimeout(rotator);
-            scrubber_handle.stop(true, false);
             i = images.length - 1;
+            handle = document.getElementById('range');
+            handle.value = handle.max  
             scrubber_handle.css('left', i*inc + 'px');
-            if(play_pause.attr('class') === 'pause') { // was playing
+            if(play_pause.attr('class') === 'pause') {
                 image_cycle();
-            } else {
+            } else { // was playing
                 set_image(images[i]);
             }
         }
-        
         function handle_fullscreen_click(e, elem) {
             e.preventDefault();
             if(!player.hasClass('full')) {
                 full = true;
-                pauseOnHover = false; // while we are in fullscreen
+                pauseOnHover = false; // while we are in fullscreen 
                 // enter fullscreen
                 player.addClass('full');
                 player.css('width', $(window).innerWidth() + 'px');
@@ -284,7 +282,7 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             } else {
                 full = false;
                 pauseOnHover = settings.pauseOnHover; // restore
-                // exit fullscreen
+                // exit fullscreen 
                 player.removeClass('full');
                 player.css({
                     width:settings.stageWidth + 'px',
@@ -301,42 +299,41 @@ if (typeof(jQuery) == 'undefined') alert('jQuery library was not found.');
             } 
         }
         
-        function handle_scrubber_click(e, elem) {
+        function handle_scrubber_drag(e, elem) {
             var pos, x_coord, delta_p, delta_n;
             e.preventDefault();
             elem = $(elem, player);
             clearTimeout(rotator);
-            scrubber_handle.stop(true, false);
-            pos = elem.offset();
-            x_coord = Math.ceil(e.pageX - pos.left);
-            i = Math.floor(x_coord / inc);
-            delta_p = Math.abs(inc*i - x_coord);
-            delta_n = Math.abs(inc*(i+1) - x_coord);
-			if(i < images.length) {
-               if(delta_p <= delta_n) {
-                	scrubber_handle.css('left', (x_coord - delta_p) + 'px');
-            	} else {
-                	scrubber_handle.css('left', (x_coord + delta_n) + 'px');
-                	if(i < images.length - 1) i++;
-            	}
-            	if(play_pause.attr('class') === 'pause') { // was playing
-                	image_cycle();
-            	} else {
-                	set_image(images[i]);
-            	}
-			}
-        }
-        
+            handle = document.getElementById('range');
+            var val = handle.value //write initial value to var 
+            handle.setAttribute("value", val); 
+            i = Number(val)
+   	    if(play_pause.attr('class') === 'pause') {
+             	image_cycle();
+   	    } else { // was playing
+               	set_image(images[i]);
+            }
+         }
+       
+         function handle_speed_up(e,elem) {
+            e.preventDefault();
+            settings.delay = settings.delay - 1/50 
+         }
+         function handle_speed_down(e,elem) {
+            e.preventDefault();
+            settings.delay = settings.delay + 1/50
+         }
     };
-    
+ 
     $.imagePlayer.settings = {
-        stageWidth:400,
-        stageHeight:300,
-        autoStart:false,
-        pauseOnHover:true,
+        stageWidth:520,
+        stageHeight:400,
+        autoStart:true,
+        pauseOnHover:false,
         delay:1,
         loop:true,
-        captions: null
+        captions: null,
+        num_images:null, //put in options in project file to overwrite max
     };
     
-})(jQuery);
+})(jQuery);	
